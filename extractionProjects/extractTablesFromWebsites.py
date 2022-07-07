e99c1add3efc24c65a8e7a8d628a1b7c44c8e7d2
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2022-07-07 15:31:46 -0700

    finished project

--- extractionProjects/extractTablesFromWebsites.py
+++ extractionProjects/extractTablesFromWebsites.py
@@ -1,3 +1,34 @@
-# This script will work on extrating tables from websites
+# This script will work on extrating tables from websites using the pandas pyton library
+# In the past we have used Beautiful Soup
+
+"""
+To demo this script we will be using the folling website:
+https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)
+
+We will be pulling all of the tables found on this page using pandas
+"""
+
+import pandas as pd
+
+# Use this command to read the html link, place the address into parantheses
+# if you use (pd.read_) you can add a different tag to use an alternative source to pull information from
+
+simpsons = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
+
+
+print(len(simpsons))
+
+
+# have to use the print command to see output onto terminal since we are not using Jupyter notebook
+print(simpsons[1])
+
+
+
+
+
+
+
+
+
 
 
