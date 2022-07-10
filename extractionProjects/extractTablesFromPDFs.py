00828616ed65a05eddec3409daa516245a7d35af
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2022-07-09 21:04:07 -0700

    update script

--- extractionProjects/extractTablesFromPDFs.py
+++ extractionProjects/extractTablesFromPDFs.py
@@ -4,6 +4,16 @@ Description:
 This python script will be able to extract tables from a pdf located in the same
 working directory with pandas library.
 
+In order to this we will be using the camelot-py library
+official documentation can be found at the following:
+https://camelot-py.readthedocs.io/en/master/
+
+after installing you will need to also add the two dependencies
+1.
+2.
+
+
 """
+
 
 
